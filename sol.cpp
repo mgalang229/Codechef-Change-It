1b25425b46076604a7fe84a23f1597e8db64f33e
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-08-31 19:19:12 +0800

    Add files via upload

---
+++ sol.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int t;
+	cin >> t;
+	while(t--) {
+		int a[100], n;
+		cin >> n;
+		for(int i=0; i<n; ++i)
+			cin >> a[i];
+		int max=*max_element(a, a+n);
+		int list[max+1];
+		for(int i=0; i<max+1; ++i)
+			list[i]=0;
+		for(int i=0; i<n; ++i)
+			list[a[i]]++;
+		int tmp=list[0];
+		for(int i=1; i<max+1; ++i)
+			if(list[i]>tmp) {
+				tmp=list[i];
+			}
+		cout << n-tmp << "\n";
+	}
+}
